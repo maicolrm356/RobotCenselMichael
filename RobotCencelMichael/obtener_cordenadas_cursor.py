092ce1c5f57b9824b0c9079e6d350956f482186b
Maicol <auxsenadesarrollo@consuerte.com.co>
2024-08-05 08:43:54 -0500

    organizacion carpetas

--- RobotCencelMichael/obtener_cordenadas_cursor.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-import time
-
-print('espere 5 segundos')
-time.sleep(5)
-
-coordenadas = pyautogui.position()
-print('Las coordenadas del cursos son: ', coordenadas)
---
+++ config.py
@@ -0,0 +1,11 @@
+import logging
+
+# BOT TELEGRAM
+TELEGRAM_TOKEN = '6232135002:AAGPl356BEAbpzSQlgomBQi45YBUZJk136Q'
+
+# LOGS
+logging.basicConfig(
+    filename='Hola.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
